1c53011f02991ff9a944ac2c8de83b5a79b963fe
Albert-Hanstein <hgunnoo@outlook.com>
2019-03-29 14:01:40 +0000

    First runs,no early stopping.

--- Python/ann.py
+++ Python/ann.py
@@ -6,6 +6,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 
 def baseline_model():
@@ -36,8 +38,27 @@ encoder = LabelEncoder()
 encoder.fit(label_names)
 encoded_labels = encoder.transform(label_names)
 one_hot_labels = np_utils.to_categorical(encoded_labels)
-# Preprocess dataframe for 15 input nodes
+# Preprocess dataframe for 15 input nodes (reduce from 4096 to 15 features)
 pca = PCA(n_components=15)
 principalComponents = pca.fit_transform(dataframe)
+features_and_labels = np.append(principalComponents, one_hot_labels, axis=1)
+np.random.shuffle(features_and_labels)
+X_train, X_test, y_train, y_test = train_test_split(features_and_labels[:, 0:15], features_and_labels[:, 15:], test_size=0.25)
 
-#TODO: Build ANN - 15,20,20,7
+
+#TODO: Implement callbacks + checkpoints
+
+
+#TODO: Train and evaluate model
+# fit model
+model = baseline_model()
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=500, verbose=1)
+# evaluate the model
+_, train_acc = model.evaluate(X_train, y_train, verbose=0)
+_, test_acc = model.evaluate(X_test, y_test, verbose=0)
+print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
+# plot training history
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.legend()
+plt.show()
